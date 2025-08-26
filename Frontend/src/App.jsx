f3f0c3c48c966b40d80762f9be3939bb9cbb817d
Saikrishna Balaka <saikrishnabalaka@gmail.com>
2025-08-26 14:03:28 +0530

    comimit by ck

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,16 +21,17 @@ function App() {
 
 async function ReviewCode() {
   try {
-    const response = await axios.post(
-      `${import.meta.env.VITE_API_BASE_URL}/ai/get-review`,
-      { code }
-    );
-    // Ensure it uses "response.data.review"
+   const response = await axios.post(
+  `${import.meta.env.VITE_API_URL}ai/get-review`,
+  { code }
+);
+    console.log("API Base URL:", import.meta.env.VITE_API_BASE_URL);
     setReview(response.data.review);
   } catch (error) {
     console.error("Error fetching review:", error);
   }
 }
+
 
 
   return (
